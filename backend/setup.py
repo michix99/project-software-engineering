9d9c38397c37266a9db234370ff3f8393e5f0ffe
Michaela Andermann <michaela.andermann@gmx.de>
2023-07-15 17:23:12 +0200

    ci: added setup file and adapted path

---
+++ backend/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="project-software-engineering",
+    version="0.0.0",
+    description="Project Software Engineering REST Backend",
+)
