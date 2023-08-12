9e8f48fe1d71737d9f440599e548541723e618ec
Glarner10 <90071630+Glarner10@users.noreply.github.com>
2023-08-12 18:26:49 +0200

    Update data_model.py
    Added more classes

--- backend/data_model.py
+++ backend/data_model.py
@@ -8,3 +8,39 @@ class Course:
 
     course_abbreviation: str
     name: str
+
+
+@dataclass
+class professor:
+    """Class to describe a professor of the IU."""
+
+    email: str
+    name_first: str
+    name_last: str
+
+
+@dataclass
+class student:
+    """Class to describe a student of the IU."""
+
+    email: str
+    name_first: str
+    name_last: str
+
+
+@dataclass
+class admin:
+    """Class to describe an admin of the IU."""
+
+    email: str
+    name_first: str
+    name_last: str
+
+
+@dataclass
+class ticket:
+    """Class to describe a ticket."""
+
+    description: str
+    course_abbreviation: str
+    title: str
